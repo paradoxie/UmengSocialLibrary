2a297b9639b3837c3cb8b49d38a878e02ccb56c4
Paradox <xzq351107836@163.com>
2016-07-09 16:18:23 +0800

    fix -提交测试library引用

--- app/src/main/java/com/test/umengsociallibrary/MainActivity.java
+++ app/src/main/java/com/test/umengsociallibrary/MainActivity.java
@@ -1,9 +1,16 @@
 package com.test.umengsociallibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.umeng.socialize.bean.RequestType;
+import com.umeng.socialize.controller.UMServiceFactory;
+import com.umeng.socialize.controller.UMSocialService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final UMSocialService mController =
+            UMServiceFactory.getUMSocialService("com.umeng.share", RequestType.SOCIAL);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
